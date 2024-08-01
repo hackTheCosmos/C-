ce4c95198e857f8a1e8afe95019cceff660e1449
HackTheCosmos44 <baileche.theo@gmail.com>
2024-08-01 19:00:28 +0200

    01/08/24

---
+++ helloWorldC++/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // input output stream
+
+using namespace std; // espace de nom
+
+int main() //fonction principale (affiche du texte dans la console)
+{
+    // cout = "c" "out", endl = retour à la ligne
+    cout << "Hello world!" << endl << "What's up ?" << endl; //affiche un message
+    return 0; // termine le programme
+}
---
+++ variablesInitiationC++/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    //déclaration de varialbes
+    int age(44);
+    string friendName("jo");
+    cout << "Hello " << friendName << " I'm " << age << " years old !" << endl;
+    return 0;
+}
